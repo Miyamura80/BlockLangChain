3f82a0d0c8f11febcec07d5f69ebe3dfef5df08a
Radoslav Dimitrov <radoslav192@gmail.com>
2023-04-15 11:40:30 +0900

    Add initial version of langchain

---
+++ backend/src/web3_config.py
@@ -0,0 +1,22 @@
+import os
+from web3 import Web3, EthereumTesterProvider
+
+
+def setup_web3(mode="infura"):
+    if mode == "infura":
+        infura_key = os.environ["INFURA_API_KEY"]
+        w3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{infura_key}"))
+    elif mode == "test":
+        w3 = Web3(EthereumTesterProvider())
+    else:
+        raise ValueError(f"Invalid web3 setup mode '{mode}'.")
+
+    assert w3.is_connected()
+    return w3
+
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["INFURA_API_KEY"] = ""
+os.environ["SERPAPI_API_KEY"] = ""
+os.environ["ETHERSCAN_API_KEY"] = ""
+
+w3 = setup_web3(mode="infura")
