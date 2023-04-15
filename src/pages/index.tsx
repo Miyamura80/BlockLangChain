a204b882ee1fcf72c19084d098f4c3d33d0fd27e
odhran-o-d <odhran.r.odonoghue@gmail.com>
2023-04-16 00:18:05 +0900

    added the new index.tsx content from main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { ethers } from 'ethers';
 import { IDKitWidget } from '@worldcoin/idkit'
 import type { ISuccessResult } from "@worldcoin/idkit";
 import dynamic from 'next/dynamic';
+import LanguageTabs from './languagetab';
 import { Message } from '@/components/Message';
 import { useAccount, useConnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected'
@@ -30,9 +31,12 @@ declare global {
 export default function Home() {
   const { connect } = useConnect({
     connector: new InjectedConnector(),
-  })
+  }) 
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [input, setInput] = useState<string>('');
+  const [metamaskAddr, setMetamaskAddr] = useState<string>('Not connected to wallet');
+  const [chatSession, setChatSession] = useState<ChatSession>([]);
+  const [language, setLanguage] = useState<string>('english');
   const { address, isConnected } = useAccount();
   const [chatSession, setChatSession] = useState([]);
   const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -44,6 +48,40 @@ export default function Home() {
   };
 
   useEffect(scrollToBottom, [messages]);
+
+  const changeLanguage = (e:any) => {
+    setLanguage(e.target.id);
+  };
+
+  const buttonClass = (lang:string) => {
+    const baseClass =
+      'hs-tab-active:bg-white hs-tab-active:text-gray-700 hs-tab-active:dark:bg-gray-800 hs-tab-active:dark:text-gray-400 dark:hs-tab-active:bg-gray-800 py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm text-gray-500 hover:text-gray-700 font-medium rounded-md hover:hover:text-blue-600 dark:text-gray-400 dark:hover:text-white';
+    return language === lang ? `${baseClass} bg-white dark:bg-gray-800` : baseClass;
+  };
+
+  useEffect(() => {
+    if (language) {
+      fetch(`http://localhost:5000/api/set_language/${language}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+        })
+        .catch((error) => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
+    }
+  }, [language]);
+
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
 
   const resetMemory = async () => {
     // Make API call to Flask backend
@@ -68,7 +106,7 @@ export default function Home() {
     if (typeof window.ethereum !== 'undefined') {
       try {
         // Request user to connect MetaMask wallet
-        await connect();
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
   
         // You can now use the connected account for further actions, e.g., getting the account balance
         // const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -78,8 +116,9 @@ export default function Home() {
         // Display connected account address in the chat
         setMessages((prevMessages) => [
           ...prevMessages,
-          { sender: 'other', text: `Connected wallet` },
+          { sender: 'other', text: `Connected to ${accounts[0]}` },
         ]);
+        setMetamaskAddr(`Connected to: ${accounts[0]}`);
       } catch (error:any) {
         // Handle errors that occurred during the connection process
         console.error('Error connecting MetaMask wallet:', error.message);
@@ -131,8 +170,6 @@ export default function Home() {
     if (e.key === 'Enter' && input.trim() !== '') {
       // Add your own message to the chat
       setMessages([...messages, { sender: 'self', text: input.trim() }]);
-
-      // @ts-ignore
       setChatSession([input.trim(), ...chatSession]);  
     }
   };
@@ -166,20 +203,43 @@ export default function Home() {
         <title>BlockLangChain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
   
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+
+
         <div className="relative w-full max-w-2xl mx-auto">
           <div className="bg-white shadow-lg rounded-2xl p-6">
             <h1 className="text-4xl font-bold text-center mb-4">
               Chat Interface
             </h1>
 
+
+
             {/* Messages */}
             <div className="overflow-y-auto h-72 mb-4">
               {messages.map((message, index) => (
-                <Message message={message} key={index}/>
-              ))}
+                <div
+                  key={index}
+                  className={`flex mb-4 ${
+                    message.sender === 'self'
+                      ? 'justify-end items-end'
+                      : 'justify-start items-start'
+                  }`}
+                >
 
+
+                  <div
+                    className={`rounded-lg px-4 py-2 mx-2 ${
+                      message.sender === 'self'
+                        ? 'bg-blue-500 text-white'
+                        : 'bg-slate-300 text-black'
+                    }`}
+                  >
+                    {message.text}
+                  </div>
+                </div>
+              ))}
               <div ref={messagesEndRef}></div>
             </div>
 
@@ -190,30 +250,69 @@ export default function Home() {
                 className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:bg-gray-500"
                 placeholder="Type your message here"
                 value={input}
-                onChange={e => setInput(e.target.value)}
+                onChange={handleInput}
                 onKeyDown={handleKeyDown}
               />
             </div>
 
             {/* MetaMask Address */}
             <div className="border-t border-gray-300 pt-4 text-black"> 
-                  {address}
+                  {metamaskAddr}
             </div>
 
-            <IDKitWidget
-              action="test-action-eito"
-              onSuccess={onSuccess}
-              handleVerify={handleProof}
-              app_id="app_staging_7dceb87587ebc8f52332d91f9a6e5280"
-              // walletConnectProjectId="get_this_from_walletconnect_portal"
-            >
-              {({ open }) => 
-                <button onClick={open} 
-                        type="button" 
-                        className="rounded-lg border border-gray-700 bg-gray-700 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-gray-900 hover:bg-gray-900 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-300">Verify Humanity</button>
+            <div className="container mx-auto">
+                <div className="flex px-4"> 
+                <IDKitWidget
+                  action="test-action-eito"
+                  onSuccess={onSuccess}
+                  handleVerify={handleProof}
+                  app_id="app_staging_7dceb87587ebc8f52332d91f9a6e5280"
+                  // walletConnectProjectId="get_this_from_walletconnect_portal"
+                >
+                  {({ open }) => 
+                    <button onClick={open} 
+                            type="button" 
+                            className="rounded-lg border border-gray-700 bg-gray-700 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-gray-900 hover:bg-gray-900 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-300">Verify Humanity</button>
 
-              }
-            </IDKitWidget>
+                  }
+                </IDKitWidget>
+                
+                <div className="flex px-4">
+      <div className="flex bg-gray-100 hover:bg-gray-200 rounded-lg transition p-1 dark:bg-gray-700 dark:hover:bg-gray-600">
+        <nav className="flex space-x-2" aria-label="Tabs" role="tablist">
+          <button
+            type="button"
+            className={buttonClass('english')}
+            id="english"
+            onClick={changeLanguage}
+          >
+            ENGLISH
+          </button>
+          <button
+            type="button"
+            className={buttonClass('japanese')}
+            id="japanese"
+            onClick={changeLanguage}
+          >
+            日本語
+          </button>
+          <button
+            type="button"
+            className={buttonClass('bulgarian')}
+            id="bulgarian"
+            onClick={changeLanguage}
+          >
+            български
+          </button>
+        </nav>
+      </div>
+    </div>
+                
+              
+              </div>
+            </div>
+            
+            
             
           </div>
           <div className="mt-8 text-center">
