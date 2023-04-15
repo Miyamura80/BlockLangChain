92095c4d926060a4c08cafd41b2fb8b7ce5a80cf
odhran-o-d <odhran.r.odonoghue@gmail.com>
2023-04-15 18:18:04 +0900

    next_secret_key_attempt

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-from flask import Flask, request, jsonify, send_from_directory, session
+from flask import Flask, request, jsonify, session
 from web3 import Web3
 from flask_cors import CORS
-from web3 import Web3, exceptions
-import configparser
+from web3 import Web3
 import os
-
-# Secrets
-# INFURA_API_TOKEN = ""
-# env_config = configparser.ConfigParser()
-# env_config.read("config.ini")
-# if env_config["DEFAULT"]["INFURA_API_TOKEN"]:
-#     INFURA_API_TOKEN = env_config["DEFAULT"]["INFURA_API_TOKEN"]
 
 INFURA_API_TOKEN = os.getenv("INFURA_API_TOKEN")
 
 app = Flask(__name__)
 CORS(app)
-
-
-# Connect to the Ethereum testnet (e.g., Rinkeby)
+app.secret_key = os.environ.get("GARBAGE_POINTER") or os.urandom(24)
 w3 = Web3(Web3.HTTPProvider(f"https://goerli.infura.io/v3/{INFURA_API_TOKEN}"))
 
 
-# create session on startup
 @app.before_first_request
 def create_session():
-    app.secret_key = os.environ.get("FLASK_SECRET_KEY")
     session["conversation"] = []
 
 
@@ -35,29 +23,9 @@ def handle_chat():
     print(data)
     text = data["text"]
     session["conversation"] += [text]
-
-    # console log the session
     print(session["conversation"])
-
-    # return jsonify(text=text)
     return jsonify(text=" ".join(session["conversation"]))
 
-
-# @app.route('/api/get_nonce', methods=['POST'])
-# def get_nonce():
-#     data = request.get_json()
-#     address = data.get('address')
-#     if not address:
-#         return jsonify({"error": "Address not provided"}), 400
-#         print("error")
-#     print(f"address successfully received. {address}")
-#     address = Web3.toChecksumAddress(address)
-#     try:
-#         nonce = w3.eth.getTransactionCount(address)
-#         return jsonify({"nonce": nonce})
-#     except Exception as e:
-#         print(e)
-#         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
