5c01c62287655e9fb5d537ec5d5a066121936c11
odhran-o-d <odhran.r.odonoghue@gmail.com>
2023-04-15 17:08:37 +0900

    attempted backend update

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ def handle_chat():
 #         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
